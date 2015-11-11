a6873c4fe6427d5fd56383721518a6c3db5f6fcd
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2015-11-11 18:35:54 +0000

    Work on week 7

--- week7/VirusComponent.java
+++ week7/VirusComponent.java
@@ -20,9 +20,17 @@ public class VirusComponent extends JComponent
 	{
 		Graphics2D g2 = (Graphics2D)g;
 		
+		//iterate over the list of proportions
 		for(int i = 0; i < proportion.length; i++)
 		{
-			
+			//iterate over the width of the frame
+			for(int j = 0; j < frameSize; j += frameSize/proportion.length)
+			{
+				// create pixel at location (i, j)
+			    Rectangle pixel = new Rectangle(i, j, 1, 1);
+			    // draw pixel using "fill" method
+			    g2.fill(pixel);
+			} 	
 		}
 	}
 }
