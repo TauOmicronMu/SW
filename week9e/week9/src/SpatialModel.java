278a2e7689b64003658ddc55a51938c1f66cf6fd
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2015-11-24 18:48:02 +0000

    Commiting work before I fall asleep on the keyboard.

---
+++ week9e/week9/src/SpatialModel.java
@@ -0,0 +1,14 @@
+/**
+ * Model of SpatialNetwork.
+ * @author txg523
+ *
+ */
+public class SpatialModel extends Observable {
+	
+	private SpatialNetwork network;
+	
+	public SpatialModel(SpatialNetwork network) {
+		super();
+		this.network = network;
+	}
+}
